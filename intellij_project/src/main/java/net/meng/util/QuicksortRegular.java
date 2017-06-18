e39d82c2220c0f34b583ea8d259999710cee4aa0
Meng Lu <lumeng.dev@gmail.com>
2017-06-18 15:42:23 -0700

    + Add display() method.

--- intellij_project/src/main/java/net/meng/util/QuicksortRegular.java
+++ intellij_project/src/main/java/net/meng/util/QuicksortRegular.java
@@ -1,9 +1,12 @@
 package net.meng.util;
 
+import java.util.Arrays;
+
 /**
  * Author: Meng LU <lumeng.dev@gmail.com>
- * Date: 9/22/14 8:44 PM
+ * Creation time: 2014-09-22T20:44:00-0800
  */
+
 
 @SuppressWarnings("unchecked")
 
@@ -14,12 +17,12 @@ public class QuickSortRegular {
         int lt = lo;
         int rt = hi+1;
 
-
         // assuming N = hi - lo + 1,
         // total comparisons A_N in partition stage is
         //
         // consider the extreme case of a sorted array,
         // A_N = 1 + hi - lo + 1 = N + 1
+
         while (true) {
             // consider the extreme case of a sorted array,
             // compareTo called for lt = lo+1
@@ -32,13 +35,42 @@ public class QuickSortRegular {
 
             if (lt >= rt) break;
 
+            display(a, lo, hi, lt, rt);
             exchange(a, lt, rt);
+            display(a, lo, hi, lt, rt);
         }
 
         exchange(a, lo, rt);
+        display(a, lo, hi, lt, rt);
 
         return rt;
 
+    }
+
+    private static void display(Comparable a[], int lo, int hi, int lt, int rt) {
+
+        String[] idx = new String[a.length];
+
+        for (int i = 0; i < idx.length; i++) {
+            idx[i] = String.format("% 6d", a[i]);
+        }
+        idx[lo] = "lo:" + String.format("% 3d", a[lo]);
+        idx[hi] = "hi:" + String.format("% 3d", a[hi]);
+        idx[lt] = "lt:" + String.format("% 3d", a[lt]);
+        idx[rt] = "rt:" + String.format("% 3d", a[rt]);
+
+        System.out.println(Arrays.toString(idx));
+    }
+
+    private static void display(Comparable a[]) {
+
+        String[] idx = new String[a.length];
+
+        for (int i = 0; i < idx.length; i++) {
+            idx[i] = String.format("% 6d", a[i]);
+        }
+
+        System.out.println(Arrays.toString(idx));
     }
 
     private static void exchange(Comparable[] a, int i, int j) {
@@ -58,20 +90,16 @@ public class QuickSortRegular {
 
     public static void main(String[] args) {
         Comparable<Integer>[] a = new Integer[10];
-
-
-        System.out.println("define array:");
+        System.out.println("initial array:");
         for (int i = 0; i<10; i++) {
             a[i] = (int) Math.ceil(100*Math.sin(i));
-            System.out.println("a[" + i + "] = " + a[i]);
         }
-
+        display(a);
+        System.out.println("sorting array:");
         sort(a, 0, 9);
 
-        System.out.println("print sorted array:");
-        for (int i = 0; i<10; i++) {
-            System.out.println("a[" + i + "] = " + a[i]);
-        }
+        System.out.println("sorted array:");
+        display(a);
 
     }
 }
